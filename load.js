b222f3b455299b7d8efd09c5606c35d1e4ed0a26
Kai Wu <wuvincentck505@gmail.com>
2020-10-06 22:11:42 +0800

    Update load.js

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
 (function(){
-	if (document.getElementsByClassName("poke-card").length > 0) {
+	if (document.getElementsByClassName("poke-card").length > 0 || document.getElementsByClassName("pokemon-container").length > 0) {
 		let link = document.createElement("link");
 		link.rel = "stylesheet";
 		link.href = "//chengkai505.github.io/PokeCard/style.css";
