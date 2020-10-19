167e332d06a99fc07fd4e40c9c704f3b4ad18035
Kai Wu <wuvincentck505@gmail.com>
2020-10-19 11:09:06 +0800

    Add Github link

--- load.js
+++ load.js
@@ -1,8 +1,14 @@
 (function(){
-	if (document.getElementsByClassName("poke-card").length > 0 || document.getElementsByClassName("pokemon-container").length > 0) {
+	let pokeCard = document.getElementsByClassName("poke-card");
+	if (pokeCard.length > 0 || document.getElementsByClassName("pokemon-container").length > 0) {
 		let link = document.createElement("link");
 		link.rel = "stylesheet";
 		link.href = "//chengkai505.github.io/PokeCard/style.css";
 		document.head.appendChild(link);
+	}
+	if (location.hostname != "kai73002981.blogspot.com") {
+		for (let i = 0; i < pokeCard.length; i++) {
+			pokeCard[i].innerHtml += '<a class="poke-github" href="https://github.com/chengkai505/PokeCard"><i class="fab fa-github"></i></a>';
+		}
 	}
 })();
