a3a7703c4f15f0e26d31d1f44f938f3936bb254c
Kai <wuvincentck505@gmail.com>
2021-06-06 16:20:47 +0800

    Update load.js

--- load.js
+++ load.js
@@ -3,7 +3,7 @@
 	if (pokeCard.length > 0 || document.getElementsByClassName("pokemon-container").length > 0) {
 		let link = document.createElement("link");
 		link.rel = "stylesheet";
-		link.href = "//chengkai505.github.io/PokeCard/style.css";
+		link.href = "//kai-tw.github.io/PokeCard/style.css";
 		document.head.appendChild(link);
 	}
 	if (location.hostname != "kai73002981.blogspot.com") {
@@ -12,7 +12,7 @@
 		link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css";
 		document.head.appendChild(link);
 		for (let i = 0; i < pokeCard.length; i++) {
-			pokeCard[i].children[1].innerHTML += '<a class="poke-github" href="https://github.com/chengkai505/PokeCard"><i class="fab fa-github"></i></a>';
+			pokeCard[i].children[1].innerHTML += '<a class="poke-github" href="https://github.com/kai-tw/PokeCard"><i class="fab fa-github"></i></a>';
 		}
 	}
 })();
